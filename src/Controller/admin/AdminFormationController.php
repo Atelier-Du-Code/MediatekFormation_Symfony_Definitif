27c8cfbdfc89c36db98d2488fbf63b8069197622
Manon Avaullée <Manon Avaulle@DESKTOP-HH8IE00>
2023-04-05 16:45:17 +0200

    Mission 2 - Tâche 1.1 : Création d'une page admin listant toutes les formations

---
+++ src/Controller/admin/AdminFormationController.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+namespace App\Controller\admin;
+
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Description of AdminFormationController
+ *
+ * @author Manon Avaullée
+ */
+class AdminFormationController extends AbstractController{
+    
+  /**
+     * 
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * 
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    const CHEMIN_FORMATION = "admin/admin.formations.html.twig";
+    
+    function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository= $categorieRepository;
+    }
+    
+    /**
+     * @Route("/admin", name="admin.formations")
+     * @return Response
+     */
+    public function index(): Response{
+        $formations = $this->formationRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(self :: CHEMIN_FORMATION,[
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+}
