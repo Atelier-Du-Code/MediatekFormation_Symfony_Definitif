d9a054fef4b8ce6ed27481af7c57be9009a7688e
Manon Avaullée <Manon Avaulle@DESKTOP-HH8IE00>
2023-05-22 01:23:06 +0200

    Mission 3 - Tâche 1.1 : Test unitaire

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -33,3 +33,5 @@ class LoginController extends AbstractController
         
     }
 }
+
+
---
+++ tests/FormationTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Tests;
+
+use App\Entity\Formation;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of FormationTest
+ *
+ * @author Manon Avaullée
+ */
+class FormationTest extends TestCase{
+    
+    public function testGetDateCreationString()
+    {
+        $formation = new Formation();
+        $formation->setPublishedAt(new \DateTime("2022-04-14"));
+        $this->assertEquals("14/04/2022", $formation->getPublishedAtString());
+    }
+}
+
